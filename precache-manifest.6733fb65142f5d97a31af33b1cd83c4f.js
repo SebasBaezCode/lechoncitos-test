b3277f9bb5ca32447fb53debcd466a9da43556f7
Sebastian Báez <40205002+SebasBaezCode@users.noreply.github.com>
2020-03-22 23:55:00 -0500

    Add files via upload

---
+++ precache-manifest.6733fb65142f5d97a31af33b1cd83c4f.js
@@ -0,0 +1,246 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a36a7bb694870d506187950eeadc0583",
+    "url": "/index.html"
+  },
+  {
+    "revision": "85152b108aa247eb6b15",
+    "url": "/static/css/main.2ed8105d.chunk.css"
+  },
+  {
+    "revision": "925ff6a9d4d84d3583bf",
+    "url": "/static/js/2.8e17867f.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.8e17867f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85152b108aa247eb6b15",
+    "url": "/static/js/main.fe674ca2.chunk.js"
+  },
+  {
+    "revision": "26747623b849e6092fd9",
+    "url": "/static/js/runtime-main.1def21d8.js"
+  },
+  {
+    "revision": "0db5fb9b5d7548e0f8969f07627b0ca6",
+    "url": "/static/media/1.0db5fb9b.jpg"
+  },
+  {
+    "revision": "ac6049f2c713ba1965a9b29378e3ed86",
+    "url": "/static/media/10.ac6049f2.jpg"
+  },
+  {
+    "revision": "863ec1cc81fd662e8a2202bd8499de2a",
+    "url": "/static/media/11.863ec1cc.jpg"
+  },
+  {
+    "revision": "b797886c9ae6e2ef5ae77eb8dbbfa15e",
+    "url": "/static/media/12.b797886c.jpg"
+  },
+  {
+    "revision": "eb84b25760b2683d5d64ea740868f05f",
+    "url": "/static/media/13.eb84b257.jpg"
+  },
+  {
+    "revision": "0f71d45e51b1b3dac9a238200e579f86",
+    "url": "/static/media/2.0f71d45e.jpg"
+  },
+  {
+    "revision": "0bc56433ff030c2abfbd8fb3ebc0cfcb",
+    "url": "/static/media/3.0bc56433.jpg"
+  },
+  {
+    "revision": "107ff7d0de8288cd0e7a636ead20375f",
+    "url": "/static/media/4.107ff7d0.jpg"
+  },
+  {
+    "revision": "a2b3fa66f4b32bf6c7d9b660105b775e",
+    "url": "/static/media/5.a2b3fa66.jpg"
+  },
+  {
+    "revision": "be7125c7aa0956e5735aeffe05b13044",
+    "url": "/static/media/6.be7125c7.jpg"
+  },
+  {
+    "revision": "4a2ad5d8408ded1660855c36fecbabe7",
+    "url": "/static/media/7.4a2ad5d8.jpg"
+  },
+  {
+    "revision": "de5a2b9a0e46373f9781882e740e80b9",
+    "url": "/static/media/8.de5a2b9a.jpg"
+  },
+  {
+    "revision": "30e73a311fbbd5f096fa0c4d6ff4e22c",
+    "url": "/static/media/9.30e73a31.jpg"
+  },
+  {
+    "revision": "01dcb77c9a956339628b370986fd6b56",
+    "url": "/static/media/Freehand-Regular.01dcb77c.ttf"
+  },
+  {
+    "revision": "d624b1462b47d714a6e87a423596857f",
+    "url": "/static/media/Overpass-Black.d624b146.ttf"
+  },
+  {
+    "revision": "d02d0d103f7b00672a5f1145c5169d8c",
+    "url": "/static/media/Overpass-Bold.d02d0d10.ttf"
+  },
+  {
+    "revision": "fe565f961d3b8a8b9d6742d5058fbeb7",
+    "url": "/static/media/Overpass-ExtraBold.fe565f96.ttf"
+  },
+  {
+    "revision": "436d40181d3b8abe47a7fb3d12972b22",
+    "url": "/static/media/Overpass-Light.436d4018.ttf"
+  },
+  {
+    "revision": "0807de33685581f680c95f790cbe3236",
+    "url": "/static/media/Overpass-Regular.0807de33.ttf"
+  },
+  {
+    "revision": "17a1468e62195d0688a6f3bd12da2e92",
+    "url": "/static/media/Overpass-SemiBold.17a1468e.ttf"
+  },
+  {
+    "revision": "36b5bab58a18b9c924861a4ccbf1a790",
+    "url": "/static/media/Roboto-Bold.36b5bab5.ttf"
+  },
+  {
+    "revision": "93b13a58dedeebe519846555a543523b",
+    "url": "/static/media/Roboto-Italic.93b13a58.ttf"
+  },
+  {
+    "revision": "9745415b8a2a8b9a2480a12f7e2d3ad3",
+    "url": "/static/media/Roboto-Medium.9745415b.ttf"
+  },
+  {
+    "revision": "5673da52c98bb6cb33ada5aaf649703e",
+    "url": "/static/media/Roboto-Regular.5673da52.ttf"
+  },
+  {
+    "revision": "5c452fb7a0f5751f4e94e9d84eb01154",
+    "url": "/static/media/SegoeScript-Regular.5c452fb7.ttf"
+  },
+  {
+    "revision": "eca5e84511ee4d8d9fa655afa4a3e125",
+    "url": "/static/media/alitas-bbq.eca5e845.png"
+  },
+  {
+    "revision": "7b69f46fd6043d05e4686dd849d79909",
+    "url": "/static/media/burger.7b69f46f.png"
+  },
+  {
+    "revision": "ba13b3463c4e708abd8ac9c30a051e41",
+    "url": "/static/media/carne-llanera.ba13b346.png"
+  },
+  {
+    "revision": "e5e750fff8317f784925577bd5201776",
+    "url": "/static/media/carne-oreada.e5e750ff.png"
+  },
+  {
+    "revision": "83ae312753c07cf21b948244d5a0d84c",
+    "url": "/static/media/cerdito-cubana.83ae3127.png"
+  },
+  {
+    "revision": "2bba142728d168b7a0147eca31d92b4c",
+    "url": "/static/media/chatas.2bba1427.png"
+  },
+  {
+    "revision": "f02759291a0db0f6b5afba8e88c68fa6",
+    "url": "/static/media/chuleta-cerdo.f0275929.png"
+  },
+  {
+    "revision": "cf809092950d9bf0a112689692386a1c",
+    "url": "/static/media/costillitas-bbq.cf809092.png"
+  },
+  {
+    "revision": "93081d89d43d51019b5e67b962b576e1",
+    "url": "/static/media/customer-review.93081d89.svg"
+  },
+  {
+    "revision": "bca129cd5bdcd0d750f4a5c3c694aaf5",
+    "url": "/static/media/default-hot-deal.bca129cd.png"
+  },
+  {
+    "revision": "b0d346220b5c4119c689d702ea5843f7",
+    "url": "/static/media/default-image.b0d34622.png"
+  },
+  {
+    "revision": "bca129cd5bdcd0d750f4a5c3c694aaf5",
+    "url": "/static/media/default-plate.bca129cd.png"
+  },
+  {
+    "revision": "4bb19fcc22a35c6847c723f0471d0faa",
+    "url": "/static/media/espaguetis-pollo.4bb19fcc.png"
+  },
+  {
+    "revision": "b09b74ebab7c25acb6dabc292ace1196",
+    "url": "/static/media/facebook.b09b74eb.svg"
+  },
+  {
+    "revision": "4efae62ee390e1770d8d52bbc4cf3f11",
+    "url": "/static/media/instagram.4efae62e.svg"
+  },
+  {
+    "revision": "d9660f5f8a46754a23295aff18d94aa8",
+    "url": "/static/media/lechona-especial.d9660f5f.png"
+  },
+  {
+    "revision": "bbfca69f1c22a0ab8d7421c9e087d57b",
+    "url": "/static/media/logo.bbfca69f.svg"
+  },
+  {
+    "revision": "7ff780e9d953ec6e2ff0c814cc3af903",
+    "url": "/static/media/logo_full.7ff780e9.svg"
+  },
+  {
+    "revision": "5cd9c2ae7e01d9d0f51e7c28515414ce",
+    "url": "/static/media/mute.5cd9c2ae.png"
+  },
+  {
+    "revision": "ffb91513a3e2d06d113d65e27690fe71",
+    "url": "/static/media/oferta.ffb91513.png"
+  },
+  {
+    "revision": "df4326422ab085c6122b55ffa42e4837",
+    "url": "/static/media/pechuga-plancha.df432642.png"
+  },
+  {
+    "revision": "05dfcabc7c9a4b368a7f2ab3e963e6ef",
+    "url": "/static/media/picada-criolla.05dfcabc.png"
+  },
+  {
+    "revision": "3b9ce3f95f66c5d4676d68f1bec7fdf6",
+    "url": "/static/media/picada-mixta.3b9ce3f9.png"
+  },
+  {
+    "revision": "cbb44e4c4bf27b1fbc079dec94af53af",
+    "url": "/static/media/picada-tipica.cbb44e4c.png"
+  },
+  {
+    "revision": "bc16a0df75d82ba897e35e53a4486068",
+    "url": "/static/media/pierna-pernil.bc16a0df.png"
+  },
+  {
+    "revision": "e3fdb2a775ac52e24e03fa8ada5f293b",
+    "url": "/static/media/point.e3fdb2a7.svg"
+  },
+  {
+    "revision": "b1b876c07f5521963100020c7ebff8b4",
+    "url": "/static/media/sobrebarriga.b1b876c0.png"
+  },
+  {
+    "revision": "907da2dafba8aa08de4492cce2332c83",
+    "url": "/static/media/test.907da2da.png"
+  },
+  {
+    "revision": "5eb905cc1074c4d06b2d8f5d6ccd6656",
+    "url": "/static/media/tipico-lechoncitos.5eb905cc.png"
+  },
+  {
+    "revision": "e8b008fd81691a3486937981ad6a83e2",
+    "url": "/static/media/whatsapp.e8b008fd.svg"
+  }
+]);
